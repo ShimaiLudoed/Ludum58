bdec79d44922e238ebb006759a048f00c9c2d30b
Shimail <MikhaiilShvets@yandex.ru>
2025-10-06 01:32:40 +0300

    Update Meteor.cs

--- Ludum58/Assets/Source/Collect/Meteor.cs
+++ Ludum58/Assets/Source/Collect/Meteor.cs
@@ -1,15 +1,33 @@
 using System;
 using UnityEngine;
 using Zenject;
+using Random = UnityEngine.Random;
 
 public class Meteor : MonoBehaviour
 {
     private LayerData _layerData;
+    [SerializeField] private Mesh[] meteorMeshes;
+    private MeshFilter meshFilter;
 
     [Inject]
     public void Construct(LayerData layerData)
     {
         _layerData = layerData;
+    }
+
+    private void Start()
+    {
+        SetupRandomAppearance();
+    }
+
+    void SetupRandomAppearance()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        
+        if (meteorMeshes != null && meteorMeshes.Length > 0)
+        {
+            meshFilter.mesh = meteorMeshes[Random.Range(0, meteorMeshes.Length)];
+        }
     }
 
     private void OnTriggerEnter (Collider other)
