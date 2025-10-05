02166fcdb89d44c2c7a04f25c77055d283eaf965
Shimail <MikhaiilShvets@yandex.ru>
2025-10-05 23:19:55 +0300

    Feat : add collect system and damage system

--- Ludum58/Assets/Source/Collect/Meteor.cs
+++ Ludum58/Assets/Source/Collect/Meteor.cs
@@ -1,18 +1,14 @@
 using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Zenject;
 
 public class Meteor : MonoBehaviour
 {
-    [SerializeField] private int damage = 20;
-    private PlayerStats _playerStats;
     private LayerData _layerData;
 
     [Inject]
-    public void Construct(LayerData layerData, PlayerStats playerStats)
+    public void Construct(LayerData layerData)
     {
-        _playerStats = playerStats;
         _layerData = layerData;
     }
 
@@ -20,11 +16,10 @@ public class Meteor : MonoBehaviour
     {
         if(LayerMaskCheck.ContainsLayer(_layerData.player, other.gameObject.layer))
         {
-            _playerStats.TakeDamage(20);
             Destroy(gameObject);
         }
     }
 
-    public class MeteorFactory : PlaceholderFactory<LayerData, PlayerStats, Meteor>
+    public class MeteorFactory : PlaceholderFactory<LayerData, Meteor>
     { }
 }
