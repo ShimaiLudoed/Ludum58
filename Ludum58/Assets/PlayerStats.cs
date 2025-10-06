3efdf3f402098dcc942c25c2fab9d5af0ea4548e
Shimail <MikhaiilShvets@yandex.ru>
2025-10-06 20:03:15 +0300

    Feat : add animation for  heal

--- Ludum58/Assets/PlayerStats.cs
+++ Ludum58/Assets/PlayerStats.cs
@@ -1,31 +1,82 @@
 using UnityEngine;
-using TMPro;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayerStats : MonoBehaviour
 {
     [Header("Игровые параметры")]
-    public int HP = 100;
+    public int maxHP = 3;
+    public int currentHP;
     
-    public TMP_Text hpText;
+    [Header("Спрайты жизней")]
+    public SpriteRenderer[] heartSprites;
 
     void Start()
     {
-        UpdateUI();
+        currentHP = maxHP;
+        UpdateHeartsUI();
     }
 
     public void TakeDamage(int amount)
     {
-        HP -= amount;
-        if (HP <= 0)
+        int previousHP = currentHP;
+        currentHP -= amount;
+        
+        if (currentHP <= 0)
         {
+            currentHP = 0;
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        UpdateUI();
+        
+        StartCoroutine(AnimateHeartLoss(previousHP));
     }
 
-    void UpdateUI()
+    IEnumerator AnimateHeartLoss(int previousHP)
     {
-        if (hpText != null) hpText.text = $"HP: {HP}";
+        for (int i = currentHP; i < previousHP && i < heartSprites.Length; i++)
+        {
+            if (heartSprites[i] != null)
+            {
+                SpriteRenderer heart = heartSprites[i];
+                
+                float duration = 0.5f;
+                float timer = 0f;
+                
+                while (timer < duration)
+                {
+                    timer += Time.deltaTime;
+                    float alpha = 1f - (timer / duration);
+                    Color color = heart.color;
+                    color.a = alpha;
+                    heart.color = color;
+                    yield return null;
+                }
+                
+                heart.enabled = false;
+            }
+        }
+    }
+
+    void UpdateHeartsUI()
+    {
+        for (int i = 0; i < heartSprites.Length; i++)
+        {
+            if (heartSprites[i] != null)
+            {
+                bool shouldBeEnabled = (i < currentHP);
+                
+                if (shouldBeEnabled)
+                {
+                    heartSprites[i].enabled = true;
+                    Color color = heartSprites[i].color;
+                    color.a = 1f;
+                    heartSprites[i].color = color;
+                }
+                else
+                {
+                    heartSprites[i].enabled = false;
+                }
+            }
+        }
     }
 }
