3efa2497eb03696a1b16ca401f3b1bdd896c1324
Shimail <MikhaiilShvets@yandex.ru>
2025-10-06 19:03:59 +0300

    Update Meteor.cs

--- Ludum58/Assets/Source/Collect/Meteor.cs
+++ Ludum58/Assets/Source/Collect/Meteor.cs
@@ -5,42 +5,64 @@ using Random = UnityEngine.Random;
 
 public class Meteor : MonoBehaviour
 {
-    [SerializeField] private int damage;
-    private LayerData _layerData;
-    private PlayerStats _playerStats;
-    [SerializeField] private Mesh[] meteorMeshes;
-    private MeshFilter meshFilter;
+  [SerializeField] private int damage;
+  [SerializeField] private float rotationSpeed = 50f;
+  [SerializeField] private Vector3 rotationAxis = Vector3.up;
+    
+  private LayerData _layerData;
+  private PlayerStats _playerStats;
+  [SerializeField] private Mesh[] meteorMeshes;
+  private MeshFilter meshFilter;
 
-    [Inject]
-    public void Construct(LayerData layerData, PlayerStats playerStats)
+  [Inject]
+  public void Construct(LayerData layerData, PlayerStats playerStats)
+  {
+    _layerData = layerData;
+    _playerStats = playerStats;
+  }
+
+  private void Start()
+  {
+    meshFilter = GetComponent<MeshFilter>();
+    SetupRandomAppearance();
+    SetupRandomRotation();
+  }
+
+  void SetupRandomAppearance()
+  {
+    if (meteorMeshes != null && meteorMeshes.Length > 0)
     {
-        _layerData = layerData;
-        _playerStats = playerStats;
+      meshFilter.mesh = meteorMeshes[Random.Range(0, meteorMeshes.Length)];
     }
+  }
 
-    private void Start()
+  void SetupRandomRotation()
+  {
+    rotationSpeed = Random.Range(30f, 80f);
+        
+    rotationAxis = new Vector3(
+      Random.Range(-1f, 1f),
+      Random.Range(-1f, 1f), 
+      Random.Range(-1f, 1f)
+    ).normalized;
+    
+    transform.rotation = Random.rotation;
+  }
+
+  private void Update()
+  {
+    transform.Rotate(rotationAxis * rotationSpeed * Time.deltaTime);
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if(LayerMaskCheck.ContainsLayer(_layerData.player, other.gameObject.layer))
     {
-       meshFilter = GetComponent<MeshFilter>();
-        SetupRandomAppearance();
+      _playerStats.TakeDamage(damage);
+      Destroy(gameObject);
     }
+  }
 
-   void SetupRandomAppearance()
-    {
-        if (meteorMeshes != null && meteorMeshes.Length > 0)
-        {
-            meshFilter.mesh = meteorMeshes[Random.Range(0, meteorMeshes.Length)];
-        }
-    }
-
-    private void OnTriggerEnter (Collider other)
-    {
-        if(LayerMaskCheck.ContainsLayer(_layerData.player, other.gameObject.layer))
-        {
-            _playerStats.TakeDamage(damage);
-            Destroy(gameObject);
-        }
-    }
-
-    public class MeteorFactory : PlaceholderFactory<LayerData, PlayerStats, Meteor>
-    { }
+  public class MeteorFactory : PlaceholderFactory<LayerData, PlayerStats, Meteor>
+  { }
 }
